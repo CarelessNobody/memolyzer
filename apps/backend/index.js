2c61ff1b245d962d53df4488979d48233cf79fb0
weng <weng2019157365@gmail.com>
2026-02-20 22:34:52 -0500

    Added template for future backend api

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -13,6 +13,29 @@ app.get("/api/test", (req, res) => {
   res.send({ id: 1, test: "This is a test response from the backend!" });
 });
 
+app.post("/api/user", (req, res) => {
+  res.send({ message: "This should create user based on data", data: req.body });
+});
+
+app.get("/api/user/:id", (req, res) => {
+  res.send({ message: "This should return user data for ID: " + req.params.id });
+});
+
+app.post("/api/flashcard", (req, res) => {
+  res.send({ message: "This should create flashcard based on data", data: req.body });
+});
+
+app.route("/api/flashcard/:id")
+  .get((req, res) => {
+    res.send({ message: "This should return flashcard data for ID: " + req.params.id });
+  })
+  .put((req, res) => {
+    res.send({ message: "This should update flashcard data for ID: " + req.params.id, data: req.body });
+  })
+  .delete((req, res) => {
+    res.send({ message: "This should delete flashcard data for ID: " + req.params.id });
+  });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

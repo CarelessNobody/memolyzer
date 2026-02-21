7c81a01251dcd85a97ccc8431dfd04352ef77efd
mouthapongariya@gmail.com <mouthapongariya@gmail.com>
2026-02-20 23:07:22 -0500

    home page draft

---
+++ apps/frontend/src/header.jsx
@@ -0,0 +1,20 @@
+export const Header = () => {
+  return (
+    <header className="navbar">
+      <ul>
+        <li className="nav-item">
+          <a href='#'>Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Upload</a>
+        </li>
+        <li className="nav-item">
+          <a href="#">Library</a>
+        </li>
+        <li className="nav-item">
+            <a href="#">Login</a>
+        </li>
+      </ul>
+    </header>
+  )
+}

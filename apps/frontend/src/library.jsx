0c6b24ef367b2edd6bb60131a955dd520fb4bf6f
weng <weng2019157365@gmail.com>
2026-02-21 00:49:06 -0500

    Added template for flashcard library

---
+++ apps/frontend/src/library.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+const CreateFlashcard = ({question, answer}) => {
+    return (
+        <div className="flashcard">
+            <h1>TEMP Flashcard</h1>
+            <input type="text" placeholder="Question" value={question} />
+            <input type="text" placeholder="Answer" value={answer} />
+        </div>
+    );
+};
+
+//TEMP!!!
+const flashcardData = [
+  {    question: "What is the capital of France?", answer: "Paris"}, 
+  {    question: "What is the capital of Germany?", answer: "Berlin"},
+  {    question: "What is the capital of Italy?", answer: "Rome"}
+];
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div className="flashcard-container">
+      {flashcardData.map((flashcard) => 
+        <CreateFlashcard key={flashcard.question} question={flashcard.question} answer={flashcard.answer} />)
+      }
+    </div>
+  </StrictMode>,
+)
+

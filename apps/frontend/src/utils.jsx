d89188136a329c85ba5980a784c891ad7ee47fad
weng <weng2019157365@gmail.com>
2026-02-20 22:54:59 -0500

    Deleted fetchTest and added more fetch functionality to utils

--- apps/frontend/src/utils.jsx
+++ apps/frontend/src/utils.jsx
@@ -2,44 +2,68 @@ import { useState, useEffect } from "react";
 
 export const baseUrl = "http://localhost:5000/api";
 
-//Backend should return JSON
 //CUSTOM HOOKS NEED TO START WITH "use"
-export const useFetch = ({url}) => {
+export const useFetchGet = ({url}) => {
+  const {data, isLoading, error} = fetchUrl({url, method: "GET", body: null});
+  return { data, isLoading, error };
+};
+
+export const useFetchPost = ({url, body}) => {
+  const {data, isLoading, error} = fetchUrl({url, method: "POST", body});
+  return { data, isLoading, error };
+};
+
+export const useFetchPut = ({url, body}) => {
+  const {data, isLoading, error} = fetchUrl({url, method: "PUT", body});
+  return { data, isLoading, error };
+};
+
+export const useFetchDelete = ({url}) => {
+  const {data, isLoading, error} = fetchUrl({url, method: "DELETE", body: null});
+  return { data, isLoading, error };
+};
+
+export const fetchUrl = ({url, method, body}) => {
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(baseUrl + url);
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const res = await fetch(baseUrl + url, {
+            method: method,
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: body ? JSON.stringify(body) : null
+          });
 
-        if (!res.ok) 
-          throw new Error("HTTPS error: " + res.status);
-        
-        const json = await res.json();
-        setData(json);
-        setError(null);
-      } catch (err) {
-        setError(err);
-        setData(null);
-        console.error("Fetch error:", err);
-      } finally {
-        setLoading(false);
-      }
-    };
+          if (!res.ok) 
+            throw new Error("HTTPS error: " + res.status);
 
-    fetchData();
-  }, []);
+          const json = await res.json();
+          setData(json);
+          setError(null);
+        } catch (err) {
+          setError(err);
+          setData(null);
+          console.error("Fetch error:", err);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchData();
+    }, []);
 
   return { data, isLoading, error };
 };
 
-
-{/* Remember! import {useFetch} from './utils.jsx' */}
+{/* Remember! import {useFetchGet, useFetchPost, useFetchPut, useFetchDelete} from './utils.jsx' */}
 
 export const TestComponent = () => {
-  const { data, isLoading, error } = useFetch({url: '/test'});
+  const { data, isLoading, error } = useFetchGet({url: '/test'});
 
   if (isLoading) 
     return <div>Loading in Progress...</div>;

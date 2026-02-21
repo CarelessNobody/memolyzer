0af766658270d3b7cc30dc8fac9a69161491fe3f
weng <weng2019157365@gmail.com>
2026-02-21 00:00:56 -0500

    Removed inital test code

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import FetchPosts from './fetchTest'
 import { Footer } from './footer'
 import { Header } from './header'
 import './App.css'
@@ -9,12 +8,10 @@ import './header.css'
 import './footer.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
-      <FetchPosts />
+
       <Footer />
     </>
   )

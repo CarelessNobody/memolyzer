cdecf6ac9bf48802a93c969d7fa4aa63659b8c54
ayuyushi <srivastavaayushi@ufl.edu>
2026-02-21 20:56:31 -0500

    removed the api key

--- apps/backend/connection.js
+++ apps/backend/connection.js
@@ -1,6 +1,5 @@
 
 // const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://root:root@memolyzer.8xgm3zo.mongodb.net/?appName=memolyzer";
 
 // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 // const client = new MongoClient(uri, {
